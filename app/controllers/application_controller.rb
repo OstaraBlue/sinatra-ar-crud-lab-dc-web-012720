807b07228905c746d8b6f2725caaa1e8d702dcd8
briannalturner <blturner3527@gmail.com>
2020-02-19 15:16:45 -0500

    CR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,29 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end 
+
+  post '/articles' do
+    @title = params[:title]
+    @content = params[:content]
+    new_a = Article.create(title: @title, content: @content)
+
+    redirect "/articles/#{new_a.id}"
   end
+
+  get '/articles/new' do
+
+    erb :new
+
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
 end
